7734d57e7d73555b6046f83454f973f745ef1f34
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-10-19 18:45:00 +0800

    Setting Up Redux for State Management

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "./providers/theme.providers";
+import { store } from "./redux/store";
 import { router } from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>
 );
