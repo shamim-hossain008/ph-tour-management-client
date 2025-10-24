f99d7f9208f314b2843e83f3d9addb2958ac8f9f
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-10-24 22:16:55 +0800

    Building the Dashboard Using ShadCN UI Blocks

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+import Logo from "@/assets/icons/Logo";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { Link } from "react-router";
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      items: [
+        {
+          title: "Analytics",
+          url: "/admin/analytics",
+        },
+      ],
+    },
+    {
+      title: "Tour Management",
+      url: "#",
+      items: [
+        {
+          title: "Add Tour",
+          url: "/admin/add-tour",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link to={item.url}>{item.title}</Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
