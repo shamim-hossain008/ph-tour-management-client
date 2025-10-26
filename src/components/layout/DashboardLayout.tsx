f8fe44879fa527d059021621fbbc200ccd1ee514
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-10-26 22:32:54 +0800

    Managing Data Display with ShadCN Table

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -20,10 +20,7 @@ export default function DashboardLayout() {
           />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <Outlet/>
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          <Outlet />
         </div>
       </SidebarInset>
     </SidebarProvider>
--- src/pages/Admin/AddTourType.tsx
+++ src/pages/Admin/AddTourType.tsx
@@ -1,10 +1,51 @@
+import { AddTourModel } from "@/components/modules/Admin/TourType/AddTourModel";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useGetTourTypeQuery } from "@/redux/features/Tour/tour.api";
+import { Trash2Icon } from "lucide-react";
 
 function AddTourType() {
   const { data } = useGetTourTypeQuery(undefined);
 
-  console.log("Get all tour type:", data);
-  return <div>Add Tour Type</div>;
+  return (
+    <div className=" max-w-7xl mx-auto px-5 w-full">
+      <div className="flex justify-between my-8">
+        <h1 className="text-xl font-semibold">Tour Types</h1>
+        <AddTourModel />
+      </div>
+      <div className="border border-muted rounded-md">
+        <Table className="min-w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Name</TableHead>
+              <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data?.data?.map((item: { name: string }) => (
+              <TableRow>
+                <TableCell className="font-medium w-full">
+                  {item?.name}
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Button size="sm">
+                    <Trash2Icon />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
 
 export default AddTourType;
